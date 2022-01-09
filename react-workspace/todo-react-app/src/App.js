437373a04697bf319c8a37ecf70e7efe536dc07c
JungHoiMin <wony1102@naver.com>
2022-01-09 22:08:28 +0900

    3.2.2 complete

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import { Paper, List } from "@material-ui/core";
+import AddTodo from './AddTodo.js';
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -8,26 +9,40 @@ class App extends React.Component {
     super(props);
     this.state = {
       items: [
-        { id:0, title: "Hello World 1", done: true },
-        { id:1, title: "Hello World 2", done: false },
+        { id:"0", title: "Hello World 1", done: true },
+        { id:"1", title: "Hello World 2", done: false },
       ],
     };
   }
 
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 리스트에 아이템 추가
+    this.setState({items: thisItems }); // 업데이트는 반드시 this.setState로 해야 됨
+    console.log("items : ", this.state.items);
+  }
+
   render() {
-    // (2) 자바스크립트가 제공하는 map 함수를 이용해 배열을 반복해 <Todo... /> 컴포넌트 생성
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin:16}}>
+      <Paper style={{ margin:16 }}>
         <List>
-          {this.state.items.map((item,idx) => (
+          {this.state.items.map((item, idx) => (
             <Todo item={item} key={item.id} />
           ))}
         </List>
       </Paper>
     );
-    
-    // (3) 생성된 컴포넌트 리턴
-    return <div className="App">{todoItems}</div>
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
