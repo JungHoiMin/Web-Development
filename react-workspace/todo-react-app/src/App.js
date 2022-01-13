f87821803f114604b5c7f33331521149f3ca74ff
JungHoiMin <wony1102@naver.com>
2022-01-13 15:33:11 +0900

    5.4.3 complete

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,26 +1,35 @@
-import React from "react";
-import Todo from "./Todo";
-import AddTodo from "./AddTodo.js";
-import { Paper, List, Container } from "@material-ui/core";
-import "./App.css";
-import { call } from "./service/ApiService";
+import React from 'react';
+import Todo from './Todo';
+import AddTodo from './AddTodo.js';
+import { Paper,
+         List,
+         Container, 
+         Grid, 
+         Button, 
+         AppBar, 
+         Toolbar, 
+         Typography
+       } from "@material-ui/core";
+import './App.css';
+import { call, signout } from "./service/ApiService";
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
       items: [],
+      loading: true,  // 로딩 중 상태를 표현할 변수
     };
   }
 
   componentDidMount() {
-    call("/todo", "GET", null).then((response) =>
-      this.setState({ items: response.data })
-    );
+    call("/todo", "GET", null).then((response) => 
+      this.setState({ items: response.data, loading: false })
+      );
   }
 
   add = (item) => {
-    call("/todo", "POST", item).then((response) =>
+    call("/todo", "POST", item).then((response) => 
       this.setState({ items: response.data })
     );
   };
@@ -32,36 +41,67 @@ class App extends React.Component {
   };
 
   update = (item) => {
-    call("/todo", "PUT", item).then((response) =>
+    call("/todo", "PUT", item).then((response) => 
       this.setState({ items: response.data })
     );
   };
 
   render() {
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{ margin: 16 }}>
+      <Paper style={{ margin:16 }}>
         <List>
           {this.state.items.map((item, idx) => (
             <Todo
               item={item}
               key={item.id}
               delete={this.delete}
-              update={this.update}
+              update={this.update}   
             />
           ))}
         </List>
       </Paper>
     );
-
-    // 3. props로 넘겨주기
-    return (
-      <div className="App">
+    
+    // navigationBar 추가
+    var navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Grid justify="space-between" container>
+            <Grid item>
+              <Typography variant="h6">오늘의 할일</Typography>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Button color="inherit" onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+    
+    /* 로딩 중이 아닐 때 렌더링 할 부분 */
+    var todoListPage = (
+      <div>
+        {navigationBar} {/* 내비게이션 바 렌더링 */}
         <Container maxWidth="md">
           <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
     );
+
+    /* 로딩 중일 때 렌더링 할 부분 */
+    var loadingPage = <h1> 로딩중.. </h1>;
+    
+    var content = loadingPage;
+    if (!this.state.loading){
+      /* 로딩 중이 아니면 todoListPage를 선택 */
+      content = todoListPage;
+    }
+
+    /* 선택한 content 렌더링 */
+    return <div className="App">{content}</div>;
   }
 }
 
